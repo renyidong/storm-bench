1df0624c0ad4ed45ee22a0bf45d940eb7ac53704
Shigeru Imai <shigeru322@gmail.com>
2016-10-11 03:18:15 -0400

    Initial commit

---
+++ src/main/java/NetworkIntensiveTopology.java
@@ -0,0 +1,139 @@
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+//import backtype.storm.utils.Utils;
+import java.util.Map;
+import java.util.UUID;
+import java.util.List;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.json.JSONObject;
+import redis.clients.jedis.Jedis;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+
+
+public class NetworkIntensiveTopology {
+    private static final Logger log = LoggerFactory.getLogger(NetworkIntensiveTopology.class);
+
+    public static class ExclamationBolt extends BaseRichBolt {
+        OutputCollector _collector;
+
+        @Override
+        public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
+            _collector = collector;
+        }
+
+        @Override
+        public void execute(Tuple tuple) {
+            _collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
+            _collector.ack(tuple);
+        }
+
+        @Override
+        public void cleanup() {
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("word"));
+        }
+    }
+
+    private static String joinHosts(List<String> hosts, String port) {
+        String joined = null;
+        for(String s : hosts) {
+            if(joined == null) {
+                joined = "";
+            }
+            else {
+                joined += ",";
+            }
+
+            joined += s + ":" + port;
+        }
+        return joined;
+    }
+
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+
+        Options opts = new Options();
+        opts.addOption("conf", true, "Path to the config file.");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(opts, args);
+        String configPath = cmd.getOptionValue("conf");
+        Map commonConfig = Utils.findAndReadConfigFile(configPath, true);
+
+        String zkServerHosts = joinHosts((List<String>)commonConfig.get("zookeeper.servers"),
+                                         Integer.toString((Integer)commonConfig.get("zookeeper.port")));
+        String kafkaTopic = (String)commonConfig.get("kafka.topic");
+        int kafkaPartitions = ((Number)commonConfig.get("kafka.partitions")).intValue();
+        int workers = ((Number)commonConfig.get("storm.workers")).intValue();
+        int ackers = ((Number)commonConfig.get("storm.ackers")).intValue();
+        int cores = ((Number)commonConfig.get("process.cores")).intValue();
+        int parallel = Math.max(1, cores/3);
+
+        ZkHosts hosts = new ZkHosts(zkServerHosts);
+
+        SpoutConfig spoutConfig = new SpoutConfig(hosts, kafkaTopic, "/" + kafkaTopic, UUID.randomUUID().toString());
+        spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+        KafkaSpout kafkaSpout = new KafkaSpout(spoutConfig);
+
+        builder.setSpout("spout", kafkaSpout, kafkaPartitions);
+        builder.setBolt("bolt1", new ExclamationBolt(), parallel)
+            .shuffleGrouping("spout");
+        builder.setBolt("bolt2", new ExclamationBolt(), parallel)
+            .allGrouping("bolt1");
+
+        Config conf = new Config();
+
+        log.info("Topology started");
+
+        if (args != null && args.length > 0) {
+            conf.setNumWorkers(workers);
+            conf.setNumAckers(ackers);
+            StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+        }
+        else {
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("test", conf, builder.createTopology());
+            backtype.storm.utils.Utils.sleep(10000);
+            cluster.killTopology("test");
+            cluster.shutdown();
+        }
+    }
+}
