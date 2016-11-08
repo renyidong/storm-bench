5c395cbe1c5810981dfff19ccdf1d20682441d44
Shigeru Imai <shigeru322@gmail.com>
2016-11-08 14:04:46 -0500

    Add WordCount and RollingSort benchmarks

---
+++ src/main/java/RollingSort.java
@@ -0,0 +1,198 @@
+import backtype.storm.Config;
+import backtype.storm.Constants;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.List;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.json.JSONObject;
+import redis.clients.jedis.Jedis;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.StringScheme;
+import storm.kafka.ZkHosts;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+
+
+public class RollingSort {
+    private static final Logger log = LoggerFactory.getLogger(RollingSort.class);
+
+    public static final String SPOUT_ID = "spout";
+    public static final String SORT_BOLT_ID ="sort";
+
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder builder = new TopologyBuilder();
+
+        Options opts = new Options();
+        opts.addOption("conf", true, "Path to the config file.");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(opts, args);
+        String configPath = cmd.getOptionValue("conf");
+        Map commonConfig = Utils.findAndReadConfigFile(configPath, true);
+        String zkServerHosts = Utils.joinHosts((List<String>)commonConfig.get("zookeeper.servers"),
+                                               Integer.toString((Integer)commonConfig.get("zookeeper.port")));
+        String kafkaTopic = (String)commonConfig.get("kafka.topic");
+        int parallel = ((Number)commonConfig.get("kafka.partitions")).intValue();
+        int workers = ((Number)commonConfig.get("storm.workers")).intValue();
+        int ackers = ((Number)commonConfig.get("storm.ackers")).intValue();
+        int cores = ((Number)commonConfig.get("process.cores")).intValue();
+        int emitFreq = ((Number)commonConfig.get("rollingsort.emitfreq")).intValue();
+        int chunkSize = ((Number)commonConfig.get("rollingsort.chunksize")).intValue();
+
+        ZkHosts hosts = new ZkHosts(zkServerHosts);
+
+        SpoutConfig spoutConfig = new SpoutConfig(hosts, kafkaTopic, "/" + kafkaTopic, UUID.randomUUID().toString());
+        spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
+        KafkaSpout spout = new KafkaSpout(spoutConfig);
+
+        builder.setSpout(SPOUT_ID, spout, parallel);
+        builder.setBolt(SORT_BOLT_ID, new SortBolt(emitFreq, chunkSize), parallel)
+            .localOrShuffleGrouping(SPOUT_ID);
+        
+        Config conf = new Config();
+
+        log.info("Topology started");
+
+        if (args != null && args.length > 0) {
+            conf.setNumWorkers(workers);
+            conf.setNumAckers(ackers);
+            StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+        }
+        else {
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("test", conf, builder.createTopology());
+            backtype.storm.utils.Utils.sleep(10000);
+            cluster.killTopology("test");
+            cluster.shutdown();
+        }
+    }
+
+    public static class SortBolt extends BaseBasicBolt {
+        public static final String FIELDS = "sorted_data";
+
+        private final int emitFrequencyInSeconds;
+        private final int chunkSize;
+        private int index = 0;
+        private MutableComparable[] data;
+
+        public SortBolt(int emitFrequencyInSeconds, int chunkSize) {
+            this.emitFrequencyInSeconds = emitFrequencyInSeconds;
+            this.chunkSize = chunkSize;
+        }
+
+        @Override
+        public void prepare(Map stormConf, TopologyContext context) {
+            this.data = new MutableComparable[this.chunkSize];
+            for (int i = 0; i < this.chunkSize; i++) {
+                this.data[i] = new MutableComparable();
+            }
+        }
+
+        public boolean isTickTuple(Tuple tuple) {
+            return tuple.getSourceComponent().equals(Constants.SYSTEM_COMPONENT_ID)
+                && tuple.getSourceStreamId().equals(Constants.SYSTEM_TICK_STREAM_ID);
+        }
+
+        @Override
+        public void execute(Tuple tuple, BasicOutputCollector basicOutputCollector) {
+            if (isTickTuple(tuple)) {
+                Arrays.sort(data);
+                basicOutputCollector.emit(new Values(data));
+                log.info("index = " + index);
+            } else {
+                Object obj = tuple.getValue(0);
+                if (obj instanceof Comparable) {
+                    data[index].set((Comparable) obj);
+                } else {
+                    throw new RuntimeException("tuple value is not a Comparable");
+                }
+                index = (index + 1 == chunkSize) ? 0 : index + 1;
+            }
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+            outputFieldsDeclarer.declare(new Fields(FIELDS));
+        }
+
+        @Override
+        public Map<String, Object> getComponentConfiguration() {
+            Map<String, Object> conf = new HashMap<String, Object>();
+            conf.put(Config.TOPOLOGY_TICK_TUPLE_FREQ_SECS, emitFrequencyInSeconds);
+            return conf;
+        }
+    }
+
+    private static class MutableComparable implements Comparable, Serializable {
+        private static final long serialVersionUID = -5417151427431486637L;
+        private Comparable c = null;
+
+        public MutableComparable() {
+
+        }
+
+        public MutableComparable(Comparable c) {
+            this.c = c;
+        }
+
+        public void set(Comparable c) {
+            this.c = c;
+        }
+
+        public Comparable get() {
+            return c;
+        }
+
+        @Override
+        public int compareTo(Object other) {
+            if (other == null) return 1;
+            Comparable oc = ((MutableComparable) other).get();
+            if (null == c && null == oc) {
+                return 0;
+            } else if (null == c) {
+                return -1;
+            } else if (null == oc) {
+                return 1;
+            } else {
+                return c.compareTo(oc);
+            }
+        }
+    }
+}
--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -5,13 +5,15 @@ import backtype.storm.spout.SchemeAsMultiScheme;
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
-//import backtype.storm.utils.Utils;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.List;
@@ -45,47 +47,12 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 
 
-public class NetworkIntensiveTopology {
-    private static final Logger log = LoggerFactory.getLogger(NetworkIntensiveTopology.class);
+public class WordCount {
+    private static final Logger log = LoggerFactory.getLogger(WordCount.class);
 
-    public static class ExclamationBolt extends BaseRichBolt {
-        OutputCollector _collector;
-
-        @Override
-        public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
-            _collector = collector;
-        }
-
-        @Override
-        public void execute(Tuple tuple) {
-            _collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
-            _collector.ack(tuple);
-        }
-
-        @Override
-        public void cleanup() {
-        }
-
-        @Override
-        public void declareOutputFields(OutputFieldsDeclarer declarer) {
-            declarer.declare(new Fields("word"));
-        }
-    }
-
-    private static String joinHosts(List<String> hosts, String port) {
-        String joined = null;
-        for(String s : hosts) {
-            if(joined == null) {
-                joined = "";
-            }
-            else {
-                joined += ",";
-            }
-
-            joined += s + ":" + port;
-        }
-        return joined;
-    }
+    public static final String SPOUT_ID = "spout";
+    public static final String SPLIT_ID = "split";
+    public static final String COUNT_ID = "count";
 
     public static void main(String[] args) throws Exception {
         TopologyBuilder builder = new TopologyBuilder();
@@ -97,28 +64,25 @@ public class NetworkIntensiveTopology {
         CommandLine cmd = parser.parse(opts, args);
         String configPath = cmd.getOptionValue("conf");
         Map commonConfig = Utils.findAndReadConfigFile(configPath, true);
-
-        String zkServerHosts = joinHosts((List<String>)commonConfig.get("zookeeper.servers"),
-                                         Integer.toString((Integer)commonConfig.get("zookeeper.port")));
+        String zkServerHosts = Utils.joinHosts((List<String>)commonConfig.get("zookeeper.servers"),
+                                               Integer.toString((Integer)commonConfig.get("zookeeper.port")));
         String kafkaTopic = (String)commonConfig.get("kafka.topic");
-        int kafkaPartitions = ((Number)commonConfig.get("kafka.partitions")).intValue();
+        int parallel = ((Number)commonConfig.get("kafka.partitions")).intValue();
         int workers = ((Number)commonConfig.get("storm.workers")).intValue();
         int ackers = ((Number)commonConfig.get("storm.ackers")).intValue();
         int cores = ((Number)commonConfig.get("process.cores")).intValue();
-        int parallel = Math.max(1, cores/3);
 
         ZkHosts hosts = new ZkHosts(zkServerHosts);
 
         SpoutConfig spoutConfig = new SpoutConfig(hosts, kafkaTopic, "/" + kafkaTopic, UUID.randomUUID().toString());
         spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme());
-        KafkaSpout kafkaSpout = new KafkaSpout(spoutConfig);
-
-        builder.setSpout("spout", kafkaSpout, kafkaPartitions);
-        builder.setBolt("bolt1", new ExclamationBolt(), parallel)
-            .shuffleGrouping("spout");
-        builder.setBolt("bolt2", new ExclamationBolt(), parallel)
-            .shuffleGrouping("bolt1");
-            // .allGrouping("bolt1");
+        KafkaSpout spout = new KafkaSpout(spoutConfig);
+        
+        builder.setSpout(SPOUT_ID, spout, parallel);
+        builder.setBolt(SPLIT_ID, new SplitSentence(), parallel)
+            .localOrShuffleGrouping(SPOUT_ID);
+        builder.setBolt(COUNT_ID, new Count(), parallel)
+            .fieldsGrouping(SPLIT_ID, new Fields(SplitSentence.FIELDS));
 
         Config conf = new Config();
 
@@ -135,6 +99,57 @@ public class NetworkIntensiveTopology {
             backtype.storm.utils.Utils.sleep(10000);
             cluster.killTopology("test");
             cluster.shutdown();
+        }
+    }
+
+    public static class SplitSentence extends BaseBasicBolt {
+
+        public static final String FIELDS = "word";
+
+        @Override
+        public void prepare(Map stormConf, TopologyContext context) {
+        }
+
+        @Override
+        public void execute(Tuple input, BasicOutputCollector collector) {
+            String[] words = (input.getString(0) != null) ? 
+                input.getString(0).split("\\s+") : null;
+            for (String word : words) {
+                collector.emit(new Values(word));
+            }
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields(FIELDS));
+        }
+
+    }
+
+    public static class Count extends BaseBasicBolt {
+        public static final String FIELDS_WORD = "word";
+        public static final String FIELDS_COUNT = "count";
+
+        Map<String, Integer> counts = new HashMap<String, Integer>();
+
+        @Override
+        public void prepare(Map stormConf, TopologyContext context) {
+        }
+
+        @Override
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+            String word = tuple.getString(0);
+            Integer count = counts.get(word);
+            if (count == null)
+                count = 0;
+            count++;
+            counts.put(word, count);
+            collector.emit(new Values(word, count));
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields(FIELDS_WORD, FIELDS_COUNT));
         }
     }
 }
